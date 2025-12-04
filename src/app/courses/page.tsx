7fa34c1a90c33beebc962bd66392e8129c3747ef
Mathilde <mathildebillard@noos.fr>
2025-12-04 13:08:26 +0100

    feat: adding pictures in /public/images and displaying components in /courses

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,22 +1,24 @@
 import CoursesCard from "@/components/CoursesCard";
 
-
 export default function Courses() {
   return (
     <div className="m-10">
       <main className=" bg-[#F4ECE2]">
         <div className="flex flex-row flex-wrap justify-between">Listes des cours
-          <CoursesCard 
+          <CoursesCard
+            image="/images/niveau-a-bulle.jpg"
             title="Poser une étagère" 
             date="19 Mai 2025" 
             description="Les étagères contre les murs sont des éléments souvent rencontrés..." 
           />
-                    <CoursesCard 
+          <CoursesCard
+            image="/images/peinture.jpg"
             title="Peindre mon plafond" 
             date="01 Avril 2025" 
             description="Une belle hauteur sous plafond c'est super, jusqu'à ce qu'il faille le..." 
           />
-                    <CoursesCard 
+          <CoursesCard 
+            image="/images/plomberie.jpg"
             title="Changer son robinet d'arrivé d'eau" 
             date="31 Octobre 2025" 
             description="Pour pouvoir intervenir sur son réseau domestique, il est pratique de..." 
--- src/components/CoursesCard.tsx
+++ src/components/CoursesCard.tsx
@@ -1,19 +1,26 @@
+import Image from "next/image";
 
 interface ICoursesCardProps {
+  image: string;
   title: string;
   date: string;
   description: string;
 }
 
-export default function Courses({title, date, description}: ICoursesCardProps) {
+export default function Courses({image, title, date, description}: ICoursesCardProps) {
   return (
-    <div className="">
-      <main className="py-25 px-5 bg-[#F4ECE2] sm:items-start">
-        <div className="text-primary border-2 rounded-md color-[">
-          <h3>{title}</h3>
-          <h4>{date}</h4>
-            <p>{description} </p>
-          <button>Voir</button>
+    <div className="max-w-300 max-h-500 ">
+      <main className="py-25 px-5 bg-background-charte sm:items-start">
+        <div className="min-h-100 h-105 w-100 flex flex-col border-3 rounded-md border-primary-red justify-between">
+          <Image src={image} alt={title} width={400} height={120} />
+          <div>
+            <h3 className="font-display-title font-bold text-primary-text mx-1">{title}</h3>
+            <h4 className="font-display-light font-light text-secondary-text mx-1">{date}</h4>
+          </div>
+          <div className="flex flex-row m-0.5">
+            <p className="font-display font-normal text-primary-text my-2 mx-1">{description} </p>
+            <button className=" self-end w-15 m-5 sticky right-0 bottom-0 px-2 border-2 rounded-md border-secondary-red bg-secondary-red text-background-charte">Voir</button>
+          </div>
         </div>
       </main>
     </div>
