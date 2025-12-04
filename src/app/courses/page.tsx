902ddffc28c05f92326e14c9cc2c60c67376bade
Mathilde <mathildebillard@noos.fr>
2025-12-04 11:00:56 +0100

    feat: add CoursesCard to Courses Page

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,15 +1,18 @@
 import CoursesCard from "@/components/CoursesCard";
 
+
 export default function Courses() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-[#F4ECE2] dark:bg-black sm:items-start">
-        <div className="text-primary">Listes des cours</div>
-        
-        <CoursesCard />
+        <div className="text-primary">Listes des cours
+          <CoursesCard 
+            title="Poser une étagère" 
+            date="19 Mai 2025" 
+            description="Les étagères contre les murs sont des éléments souvent rencontrés..." 
+          />
 
-      
-
+        </div>
       </main>
     </div>
   );
--- src/components/CoursesCard.tsx
+++ src/components/CoursesCard.tsx
@@ -1,18 +1,22 @@
 import Image from "next/image";
 
-export default function CoursesCard() {
+interface ICoursesCardProps {
+  title: string;
+  date: string;
+  description: string;
+}
+
+export default function Courses({title, date, description}: ICoursesCardProps) {
   return (
-    <div>
-      <Image
-        src=
-        alt="Sample Course"
-      />
-      <h2 className="mt-4 text-2xl font-bold text-gray-800 dark:text-gray-200">
-      Titre
-      </h2>
-      <p className="mt-2 text-gray-600 dark:text-gray-400">
-      Description
-      </p>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-[#F4ECE2] dark:bg-black sm:items-start">
+        <div className="text-primary border-2 rounded-md color-[">Listes des cours
+          <h3>{title}</h3>
+          <h4>{date}</h4>
+            <p>{description} </p>
+          <button>Voir</button>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
