71d00d9c7dc28168a023f40bca413bd742e6f01c
Mathilde <mathildebillard@noos.fr>
2025-12-04 13:24:31 +0100

    style: displaying 6 cards & box shadow

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -4,8 +4,26 @@ export default function Courses() {
   return (
     <div className="m-10">
       <main className=" bg-[#F4ECE2]">
-        <div className="flex flex-row flex-wrap justify-between">Listes des cours
+        <div className="flex flex-row flex-wrap justify-center ">
           <CoursesCard
+            image="/images/niveau-a-bulle.jpg"
+            title="Poser une étagère" 
+            date="19 Mai 2025" 
+            description="Les étagères contre les murs sont des éléments souvent rencontrés..." 
+          />
+          <CoursesCard
+            image="/images/peinture.jpg"
+            title="Peindre mon plafond" 
+            date="01 Avril 2025" 
+            description="Une belle hauteur sous plafond c'est super, jusqu'à ce qu'il faille le..." 
+          />
+          <CoursesCard 
+            image="/images/plomberie.jpg"
+            title="Changer son robinet d'arrivé d'eau" 
+            date="31 Octobre 2025" 
+            description="Pour pouvoir intervenir sur son réseau domestique, il est pratique de..." 
+          />
+                    <CoursesCard
             image="/images/niveau-a-bulle.jpg"
             title="Poser une étagère" 
             date="19 Mai 2025" 
--- src/components/CoursesCard.tsx
+++ src/components/CoursesCard.tsx
@@ -11,7 +11,7 @@ export default function Courses({image, title, date, description}: ICoursesCardP
   return (
     <div className="max-w-300 max-h-500 ">
       <main className="py-25 px-5 bg-background-charte sm:items-start">
-        <div className="min-h-100 h-105 w-100 flex flex-col border-3 rounded-md border-primary-red justify-between">
+        <div className="min-h-100 h-105 w-100 flex flex-col border-4 rounded-md border-primary-red justify-between shadow-xl/30">
           <Image src={image} alt={title} width={400} height={120} />
           <div>
             <h3 className="font-display-title font-bold text-primary-text mx-1">{title}</h3>
@@ -19,7 +19,7 @@ export default function Courses({image, title, date, description}: ICoursesCardP
           </div>
           <div className="flex flex-row m-0.5">
             <p className="font-display font-normal text-primary-text my-2 mx-1">{description} </p>
-            <button className=" self-end w-15 m-5 sticky right-0 bottom-0 px-2 border-2 rounded-md border-secondary-red bg-secondary-red text-background-charte">Voir</button>
+            <button type="button" className=" self-end w-15 m-5 sticky right-0 bottom-0 px-2 border-2 rounded-md border-secondary-red bg-secondary-red text-background-charte cursor-pointer">Voir</button>
           </div>
         </div>
       </main>
