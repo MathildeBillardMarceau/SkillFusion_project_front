025ea21a7ec193a4391275053d301b0bc0fe11d6
Mathilde <mathildebillard@noos.fr>
2025-12-04 10:36:25 +0100

    fix: working on a new branch for CoursesCard feature

--- src/components/CoursesCard.tsx
+++ src/components/CoursesCard.tsx
@@ -3,7 +3,16 @@ import Image from "next/image";
 export default function CoursesCard() {
   return (
     <div>
-      
+      <Image
+        src=
+        alt="Sample Course"
+      />
+      <h2 className="mt-4 text-2xl font-bold text-gray-800 dark:text-gray-200">
+      Titre
+      </h2>
+      <p className="mt-2 text-gray-600 dark:text-gray-400">
+      Description
+      </p>
     </div>
   )
 }
