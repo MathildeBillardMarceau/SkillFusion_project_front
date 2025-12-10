92b13c5a821093bab86117de8ce25e65fbb467b0
Mathilde <mathildebillard@noos.fr>
2025-12-10 12:03:14 +0100

    feat: formatting date & deconnexion button

--- src/app/utils/date.ts
+++ src/app/utils/date.ts
@@ -1,10 +1,14 @@
 export default function formatDate(dateInput: any): string {
-  
+
+  const date = new Date(dateInput);
+  if (!(date instanceof Date) || isNaN(date.getTime())) {
+    return "Invalid date"; // or return ""
+  }
   return Intl.DateTimeFormat
     ('fr-FR', {
       year: 'numeric',
       month: 'long',
       day: '2-digit',
     })
-    .format(new Date(dateInput));
+    .format(date);
 };
--- src/components/LoginPopIn.tsx
+++ src/components/LoginPopIn.tsx
@@ -13,6 +13,8 @@ export default function LoginPopIn({ onClose }: ILoginPopInProps) {
 
   const login = useAuthStore((state) => state.login);
 
+  const logout = useAuthStore((state) => state.logout);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");     
 
@@ -69,7 +71,7 @@ export default function LoginPopIn({ onClose }: ILoginPopInProps) {
     <div className="min-h-90 h-100 w-150 md:w-100">
       <p></p>
     </div>
-    <div className="min-h-90 h-90 w-60 md:w-100 flex flex-col border-4 rounded-md border-primary-red justify-between shadow-xl/30 bg-primary-red">
+    <div className="min-h-90 h-100 w-70 md:w-110 flex flex-col border-4 rounded-md border-primary-red justify-between shadow-xl/30 bg-primary-red">
       <div className="flex self-end">
         <button className="m-2 cursor-pointer" onClick={onClose}>
           <p className="text-background-charte ">
@@ -88,12 +90,21 @@ export default function LoginPopIn({ onClose }: ILoginPopInProps) {
         <input name="email" type="email" className="mb-3 p-2 rounded-md border-2 border-background-charte bg-background-charte" onChange={(event) => setEmail(event.target.value)}/>
         <label className="font-display font-normal text-background-charte mb-1">Mot de Passe</label>
         <input name="password" type="password" className="mb-3 p-2 rounded-md border-2 border-background-charte bg-background-charte outline-hidden" onChange={(event) => setPassword(event.target.value)}/>
-        <button type="submit" className=" self-end w-30 px-2 border-2 rounded-md border-secondary-red bg-secondary-red text-background-charte cursor-pointer ">Se connecter</button>
+        <div className="flex justify-around">
+        <button type="button" className=" self-end w-30 px-2 border-2 rounded-md border-secondary-red bg-secondary-red text-background-charte cursor-pointer " onClick={() => {
+          logout();
+          onClose();
+        }}>déconnexion</button>
+        <button type="submit" className=" self-end w-30 px-2 border-2 rounded-md border-secondary-red bg-secondary-red text-background-charte cursor-pointer ">connexion</button>
+        </div>
+        <div className="flex justify-around ml-5">
+        <button type="submit" className=" self-end w-40 m-4 px-2 border-2 rounded-md border-secondary-red bg-secondary-red text-background-charte cursor-pointer ">Créer un compte</button>
+        </div>
       </form>
       </div>
     </div>
     <div className="min-h-90 h-90 w-150 md:w-100">
-      <p></p>
+
     </div>
     </div>
   )
