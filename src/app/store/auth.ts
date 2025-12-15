bb2d8feb0f2094f03a909f6682d08e0f3ee83fa2
Mathilde <mathildebillard@noos.fr>
2025-12-15 12:43:15 +0100

    feat: adding modal to update userinfos

--- src/app/store/auth.ts
+++ src/app/store/auth.ts
@@ -2,42 +2,57 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 interface User {
-	id: string;
-	email: string;
-	password: string;
-	firstName: string;
-	lastName: string;
+  id: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
 }
+
 interface AuthState {
-	isAuthenticated: boolean;
-	user: User;
-	login: (user: User, accessToken: string) => void;
-	logout: () => void;
+  token: string | null;
+  isAuthenticated: boolean;
+  user: User;
+  login: (user: User, accessToken: string) => void;
+  logout: () => void;
 }
 
 export const useAuthStore = create<AuthState>()(
-	persist(
-		(set) => ({
-			token: null,
-			isAuthenticated: false,
-			user: { id: "", email: "", password: "", firstName: "", lastName: "" },
-			login: (user, accessToken) =>
-				set(() => ({ token: accessToken, isAuthenticated: true, user })),
-			logout: () =>
-				set(() => ({
-					token: null,
-					isAuthenticated: false,
-					user: {
-						id: "",
-						email: "",
-						password: "",
-						firstName: "",
-						lastName: "",
-					},
-				})),
-		}),
-		{
-			name: "auth-store",
-		},
-	),
+  persist(
+    (set) => ({
+      token: null,
+      isAuthenticated: false,
+
+      user: {
+        id: "",
+        email: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+      },
+
+      login: (user, accessToken) =>
+        set({
+          token: accessToken,
+          isAuthenticated: true,
+          user,
+        }),
+
+      logout: () =>
+        set({
+          token: null,
+          isAuthenticated: false,
+          user: {
+            id: "",
+            email: "",
+            password: "",
+            firstName: "",
+            lastName: "",
+          },
+        }),
+    }),
+    {
+      name: "auth-store",
+    }
+  )
 );
