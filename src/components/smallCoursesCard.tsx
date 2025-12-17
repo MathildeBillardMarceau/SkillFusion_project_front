e84bb662455afeacf15cc522108545cda2a773bb
Mathilde <mathildebillard@noos.fr>
2025-12-17 16:33:28 +0100

    fix: fixing path to coursesSlug from dashboard

--- src/components/smallCoursesCard.tsx
+++ src/components/smallCoursesCard.tsx
@@ -14,7 +14,8 @@ export default function SmallCoursesCard({image, title, slug}: ISmallCoursesCard
         <div className="flex flex-row h-10 w-60 md:w-100 justify-between">
           <Image src={image} alt={title} width={60} height={10} className="object-cover border-2 rounded-md border-primary-red shadow-xl/30"/>
           <h1 className="font-display-title font-bold text-xs text-primary-text mx-3 text-left">{title}</h1>
-          <Link href={slug} className="text-xs justify-items-end text-secondary-text"> Lien -&gt; </Link>
+          <Link href={{
+            pathname: "/courses/" + `${slug}`,}} className="text-xs justify-items-end text-secondary-text"> Lien -&gt; </Link>
         </div>
       </main>
       </div>
